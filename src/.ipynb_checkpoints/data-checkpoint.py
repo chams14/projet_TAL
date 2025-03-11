77732b62326499488aa198b9c85f21549e69e574
chams14 <maelle.lelannic14@gmail.com>
2025-03-11 15:52:47 +0100

    rename file

--- src/.ipynb_checkpoints/data-checkpoint.py
+++ src/.ipynb_checkpoints/data-checkpoint.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 df = pd.read_csv("./data/train_all.csv")
 
-# 50% train data = new train file 
-train = int(len(df) * 0.80)
+# 90% train data = new train file 
+train = int(len(df) * 0.90)
 df_train = df.iloc[:train]
 df_train.to_csv("./data/train.csv", index=False)
 
-# 20% train data = validation
+# 10% train data = validation
 validation = len(df)
 df_validation = df.iloc[train:validation]
 df_validation.to_csv("./data/validation.csv", index=False)
